ea44fdd2c70966df8e162c53d65c437a364f6069
Dmitry Morozov <shtrih0@gmail.com>
2023-03-26 05:52:48 +0400

    lint

--- repositories/errors.go
+++ repositories/errors.go
@@ -10,15 +10,11 @@ func NewNotFoundError(entityName string) *NotFoundError {
 	return &NotFoundError{entityName: entityName}
 }
 
-func (m *NotFoundError) Error() string {
-	return fmt.Sprintf("%s not found", m.entityName)
+func (e *NotFoundError) Error() string {
+	return fmt.Sprintf("%s not found", e.entityName)
 }
 
 func (e *NotFoundError) Is(err error) bool {
 	_, ok := err.(*NotFoundError)
-	if !ok {
-		return false
-	}
-
-	return true
+	return ok
 }
